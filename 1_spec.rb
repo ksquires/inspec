6342c0e5e0e7263f476440767881fbd58c9c7a96
Kenton Squires <ksquires@gmail.com>
2017-07-24 23:17:45 +0000

    added stuffs

--- 1_spec.rb
+++ 1_spec.rb
@@ -1,3 +1,15 @@
+control "cis-1-1-1" do
+  impact 1.0
+  title "Ensure mounting of cramfs filesystems is disabled (Scored)"
+  desc "The cramfs filesystem type is a compressed read-only Linux filesystem embedded in small footprint systems. A cramfs image can be used without having to first decompress the
+image."
+  describe command('modprobe -n -v cramfs') do
+    its('stdout') { should match (/install \/bin\/true/) }
+  end
+  describe command('lsmod | grep cramfs') do
+    its('stdout') { should match (//) }
+  end
+end
 control "cis-1-2-2" do
   impact 1.0
   title "Ensure GPG keys are configured (Not Scored)"
